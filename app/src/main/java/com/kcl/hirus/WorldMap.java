ce1a9c58a4061453d5af9879b6de77e247f88183
이주광 <sute__@naver.com>
2021-01-07 16:27:56 +0900

    지도  퍼즐 추가

--- app/src/main/java/com/kcl/hirus/WorldMap.java
+++ app/src/main/java/com/kcl/hirus/WorldMap.java
@@ -11,12 +11,14 @@ import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ScrollView;
+import android.widget.Toast;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -32,9 +34,33 @@ public class WorldMap extends Fragment implements MainActivity.OnBackpressedList
     ImageView[] countries = new ImageView[200];
     //ImageView map;
     BitmapDrawable bitmap;
-    int RS = 0;
-    int CA = 1;
-    int US = 2;
+    static int CA = 0;//캐나다 CANADA
+    static int US = 1;//미국 US
+    static int GL = 2;//그린란드 GREENLAND
+    static int MX = 3;//멕시코 MEXICO
+    static int GT = 4;//과테말라 GUATEMALA
+    static int HD = 5;//혼두라스 HONDURAS
+    static int ELS = 6;//엘살바도르 EL SALVADOR
+    static int BLZ = 7;//벨리제 BELIZE
+    static int NCG = 8;//니카라구아 NICARAGUA
+    static int CR = 9;//코스타리카 COSTA RICA
+    static int PNM = 10;//파나마 PANAMA
+    static int CB = 11;//콜롬비아 COLOMBIA
+    static int VZ = 12;//베네수엘라 VENEZUELA
+    static int ECD = 13;//에콰도르 ECUADOR
+    static int PR = 14;//페루 PERU
+    static int BZ = 15;//브라질 BRAZIL
+    static int BV = 16;//볼리비아 BOLIVIA
+    static int PRG = 17;//파라과이 PARAGUAY
+    static int CL = 17;//칠레 CHILE
+    static int AG = 18;//아르헨티나 ARGENTINA
+    static int UG = 19;//우루과이 URUGUAY
+    static int GA = 20;//가이아나 GUYANA
+    static int SN = 21;//수리남 SURINAME
+    static int FGA = 22;//프랑스령 기아나 FRENCH GUYANA
+
+
+
 
 
     HashMap<ImageView, Integer> countryMap = new HashMap<>();
@@ -42,33 +68,25 @@ public class WorldMap extends Fragment implements MainActivity.OnBackpressedList
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View rootview = inflater.inflate(R.layout.fragment_world_map,container,false);
+        final View rootview = inflater.inflate(R.layout.fragment_world_map,container,false);
 
         scrollView = rootview.findViewById(R.id.WhitemapView);
 
         scrollView.setHorizontalScrollBarEnabled(true);
 
+        countries[CA] = rootview.findViewById(R.id.CA);
+        countries[US] = rootview.findViewById(R.id.US);
+        countries[GL] = rootview.findViewById(R.id.GL);
+        countries[MX] = rootview.findViewById(R.id.MX);
 
-        //map = rootview.findViewById(R.id.map);
-        countries[0] = rootview.findViewById(R.id.CA);
-        countries[1] = rootview.findViewById(R.id.US);
-
-
-        int j = 25;
-        for(int i = 0; i < countries.length; i++){
-
-            countryMap.put(countries[i],j);
-            j = j + 25;
-        }
-
-        for(int i = 0; i< countryMap.size(); i++){
-            if(countryMap.get(countries[i]) >= 25 & countryMap.get(countries[i]) <= 49){
-                countries[0].setColorFilter(Color.parseColor("#0000FF"), PorterDuff.Mode.SRC_IN);
+        countries[CA].setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Toast.makeText(rootview.getContext(), "캐나다", Toast.LENGTH_SHORT).show();
+                return false;
             }
-            else if(countryMap.get(countries[i]) >= 50 & countryMap.get(countries[i]) <= 74){
-                countries[1].setColorFilter(Color.parseColor("#FF0000"), PorterDuff.Mode.SRC_IN);
-            }
-        }
+        });
+
 
         return rootview;
     }
